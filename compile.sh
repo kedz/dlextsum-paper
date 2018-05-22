0214b5ff99707533dc17c49325bfb6c43ae6497e
kedz <kedzie@cs.columbia.edu>
2018-05-22 01:47:20 -0400

    CK edits.

--- compile.sh
+++ compile.sh
@@ -1,10 +1,10 @@
 cd figures
-pdflatex avgsentencoder.tex
-pdflatex rnnsentencoder.tex
-pdflatex cnnsentencoder.tex
+#pdflatex avgsentencoder.tex
+#pdflatex rnnsentencoder.tex
+#pdflatex cnnsentencoder.tex
 pdflatex s2s_extractor.tex
-pdflatex rnnextractor.tex
-pdflatex clextractor.tex
+#pdflatex rnnextractor.tex
+#pdflatex clextractor.tex
 pdflatex sr_extractor.tex
 cd ..
 pdflatex dlextsum.emnlp18.tex
