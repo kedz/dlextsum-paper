510bb561a5b418969dea4a43471a24c59906c2ef
kedz <kedzie@cs.columbia.edu>
2018-08-23 15:28:42 -0400

    Fixed some typos. Made changes in response to reviewer feedback.

--- compile.sh
+++ compile.sh
@@ -3,8 +3,8 @@ cd figures
 #pdflatex rnnsentencoder.tex
 #pdflatex cnnsentencoder.tex
 pdflatex s2s_extractor.tex
-#pdflatex rnnextractor.tex
-#pdflatex clextractor.tex
+pdflatex rnnextractor.tex
+pdflatex clextractor.tex
 pdflatex sr_extractor.tex
 cd ..
 pdflatex dlextsum.emnlp18.tex
